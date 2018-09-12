6aaef5d1e367cbc4ad82f8a51fa36cdb80e91289
Dave <duy27295@gmail.com>
2018-09-12 16:57:28 +1000

    Add notes for home agent

---
+++ EnergyTradingSystem/src/EnergyAgents/HomeAgent.java
@@ -0,0 +1,11 @@
+package EnergyAgents;
+
+import jade.core.Agent;
+
+/**
+ * This agent will calculate the demand/ supplys, negotiate with Retailer agent and decide to choose the right offers.
+ */
+public class HomeAgent extends Agent
+{
+   
+}
---
+++ EnergyTradingSystem/src/EnergyAgents/HomeReporterAgent.java
@@ -0,0 +1,14 @@
+package EnergyAgents;
+
+
+import jade.core.Agent;
+
+
+/**
+ * This agent will get the all the demands from AAs and then combine and send the total demand to home agent for further actions
+ */
+
+public class HomeReporterAgent extends Agent
+{
+    
+}
