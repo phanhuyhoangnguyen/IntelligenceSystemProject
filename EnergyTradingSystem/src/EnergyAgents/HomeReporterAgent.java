9350fb336ff36fbc9c4ae5c3a91b2c7c8f34128a
Dave <duy27295@gmail.com>
2018-09-12 16:58:21 +1000

    Add Home Reporter Agent

--- EnergyTradingSystem/src/EnergyAgents/HomeReporterAgent.java
+++ EnergyTradingSystem/src/EnergyAgents/HomeReporterAgent.java
@@ -1,8 +1,11 @@
 package EnergyAgents;
 
+import java.util.Iterator;
 
+import jade.core.AID;
 import jade.core.Agent;
-
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
 
 /**
  * This agent will get the all the demands from AAs and then combine and send the total demand to home agent for further actions
@@ -10,5 +13,42 @@ import jade.core.Agent;
 
 public class HomeReporterAgent extends Agent
 {
-    
+    //The total demand
+    private float totalDemand = 14;
+
+    protected void setup(){
+        System.out.println(getLocalName() + ": I have been created");
+        //Add behaviours
+        addBehaviour(new SendTotalDemand());
+    }
+
+    //Send the total demand to Home Agent
+    private class SendTotalDemand extends CyclicBehaviour{
+        SendTotalDemand(){
+            System.out.println(getBehaviourName() + ": I have been created.");
+        }
+
+        public void action(){
+            
+            if(totalDemand != 0){
+                ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                msg.setContent(Float.toString(totalDemand));
+                msg.addReceiver(new AID("HomeAgent", AID.ISLOCALNAME ));
+
+                //Send message (only once)
+                System.out.println(getLocalName()+ ": Sending message " + msg.getContent() + " to ");
+
+                Iterator receivers = msg.getAllIntendedReceiver();
+                while(receivers.hasNext()){
+                    System.out.println(((AID)receivers.next()).getLocalName());
+                }
+
+                send(msg);
+
+                totalDemand = 0;
+            }
+            block();
+        }
+    }
+
 }
