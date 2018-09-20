65de9fdfb8496c52ad9b20259db9f388b8261952
Dave <duy27295@gmail.com>
2018-09-20 18:29:55 +1000

    Implement DF functions

--- EnergyTradingSystem/src/EnergyAgents/HomeAgent.java
+++ EnergyTradingSystem/src/EnergyAgents/HomeAgent.java
@@ -3,18 +3,102 @@ package EnergyAgents;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.*;
+import jade.domain.FIPAException;
+
 
 /**
- * This agent will calculate the demand/ supplys, negotiate with Retailer agent and decide to choose the right offers.
+ * Home Agent 
+ * @author The 'Dangerous' Dave
+ * 
+ * @Description This agent will calculate the demand/ supplys, negotiate with Retailer agent and decide to choose the right offers.
  */
+
 public class HomeAgent extends Agent
 {
+    //Variables for agents (Dynamic)
+    private float totalEnergyConsumption;
+    private float budgetLimit;
+    private String agentName;
+    private String agentType;
+
+    /**
+     * Initialize value for home agent
+     */
+    private void init()
+    {
+        this.totalEnergyConsumption = 1000;
+        this.budgetLimit = 75;
+        agentName = "Home";
+        agentName = "Home";
+    }
+
+    public HomeAgent()
+    {
+        init();
+    }
+    /**
+     * Set Up Home Agent
+     */
+    @Override
     protected void setup(){
-        System.out.println(getLocalName() + ": created");
+        this.agentName += "_" + getAID().getLocalName();
+        System.out.println(agentName + ": created.");
+        
+        //Register the service
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType(this.agentType);
+        sd.setName(this.agentName);
+        register(sd);
+        
+
         //Add behaviours
         addBehaviour(new ReceiveTotalDemand());
     }
 
+    /**
+     * Clean up when agent die
+     */
+    @Override
+    protected void takeDown()
+    {
+        //Deregister from DF
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            //TODO: handle exception
+            fe.printStackTrace();
+        }
+
+        System.out.println(agentName + ": closed.");
+    }
+    /*---- Ultility methods to access DF ---- */
+    
+    /**
+     * Test and remove old duplicate DF entries before add new one
+     * @param sd
+     */
+    void register(ServiceDescription sd)
+    {
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        try {
+            DFAgentDescription list[] = DFService.search(this, dfd);
+            if(list.length > 0){
+                DFService.deregister(this);
+            }
+            dfd.addServices(sd);
+            DFService.register(this, dfd);      
+        } catch (FIPAException fe) {
+            //TODO: handle exception
+            fe.printStackTrace();
+        }
+    }
+
+    /**
+     * Receive demand from Appliances
+     */
     private class ReceiveTotalDemand extends CyclicBehaviour{
         public void action(){
             System.out.println(getLocalName() + ": waiting for message");
