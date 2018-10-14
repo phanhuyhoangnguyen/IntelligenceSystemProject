f079d1068c9157c6f53c61144727b09b40b4b274
Tola Veng <7678886@student.swin.edu.au>
2018-10-14 22:28:56 +1100

    Add GUI print html agent

---
+++ EnergyTradingSystem/src/GUI/PrintGUI.java
@@ -0,0 +1,98 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JDialog;
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Style;
+import javax.swing.text.StyleContext;
+import javax.swing.text.StyledDocument;
+
+import EnergyAgents.PrintAgent;
+import GUI.RetailerGUIDetails.RetailerFrame;
+
+public class PrintGUI {
+	
+	private PrintAgent myAgent;
+	private StringBuilder printText;
+	
+	private JEditorPane editorPane;
+	
+	public PrintGUI ( PrintAgent printAgent ) {
+		myAgent = printAgent;
+		printText = new StringBuilder();
+		editorPane = new JEditorPane();
+	}
+	
+	public void clearText() {
+		printText.setLength(0);
+		printText = new StringBuilder();
+		editorPane.setText("");
+	}
+	
+	public void append(String text) {
+		printText.append(text + "<br>\n\r ");
+		editorPane.setText(printText.toString());
+	}
+	
+	public void showGUI() {
+		EventQueue.invokeLater(new Runnable() {
+	         public void run() {
+	        	 PrintFrame frame = new PrintFrame();
+	        	 frame.setVisible(true);
+	         }
+	      });
+	}
+	
+	private class PrintFrame extends JFrame {
+		private String title = "Print Agent: " + myAgent.getLocalName();
+		
+		public PrintFrame() {
+			
+			setTitle(title);
+			setResizable(false);
+			
+			JLabel lblTitle = new JLabel(title);
+			
+			
+			editorPane = new JEditorPane();
+			editorPane.setEditable(false);
+			editorPane.setContentType("text/html");
+			editorPane.setText(printText.toString());
+			
+			JScrollPane scrollPan = new JScrollPane(editorPane);
+			
+			
+			// add to frame
+			Container pane = getContentPane();
+			pane.add(lblTitle, BorderLayout.NORTH);
+			pane.add(scrollPan, BorderLayout.CENTER);
+			
+			setSize(420, 720);
+			setLocationRelativeTo(null);
+			
+			// remove the agent when close	
+			addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent e) {
+					myAgent.doDelete();
+				}
+			} );
+			
+			setResizable(true);
+			
+		}
+		
+	}
+	
+}
