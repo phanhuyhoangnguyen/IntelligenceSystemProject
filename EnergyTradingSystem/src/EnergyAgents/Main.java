3b9f7623214ea9ec23e11c637f6da7631ad48c3f
Tola Veng <7678886@student.swin.edu.au>
2018-10-15 22:19:37 +1100

    Update Main

--- EnergyTradingSystem/src/EnergyAgents/Main.java
+++ EnergyTradingSystem/src/EnergyAgents/Main.java
@@ -1,18 +1,22 @@
 package EnergyAgents;
 
+import java.awt.EventQueue;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.omg.PortableServer.POAManagerPackage.State;
+
 import jade.wrapper.AgentController;
+import jade.wrapper.AgentState;
 import jade.wrapper.ContainerController;
-
+import jade.wrapper.StaleProxyException;
 import EnergyAgents.RetailerAgent;
-
-
+import GUI.MainGUI;
 import database.DbHelper;
 
 public class Main {
@@ -20,30 +24,61 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Main Class running.");
 		
-		// start Jade Platform
+		 // Start Jade Platform
 		JadeController.start();
 		
-		// start retailer agents
-		createRetailerAgent();
+		// Create Print GUI
+		createPrintAgent();
+		
+		// Create retailer agents
+		List<AgentController> retailerAgents = createRetailerAgents();
 		
 		//start home agent
 		createHomeAgent();
+		
+		// Create retailer Test agent
+		createRetailerTest();
+		
+		
+		// Start Main GUI
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainGUI mainGUI = new MainGUI();
+					
+					// attach the agents list
+					mainGUI.setRetailerAgents(retailerAgents);
+					
+					mainGUI.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
 		
 		System.out.println("Main Class finished.");
 	}
 	
 	
-	private static void createRetailerAgent() {
-		System.out.println("Retailer Agent start");
+	private static List<AgentController> createRetailerAgents() {
+		System.out.println("Retailer Agenet start");
 		
-		
+		// create container
 		ContainerController retailerContainer = JadeController.createContainer("Retailer-Container");
 		
-		AgentController retailerAgent = JadeController.createAgent("AAA", "EnergyAgents.RetailerAgent", retailerContainer);
+		// create agents
+		List<AgentController> retailerAgents= new ArrayList<>();
+		retailerAgents.add( JadeController.createAgent("Today Energy", "EnergyAgents.RetailerAgent", retailerContainer) );
+		retailerAgents.add( JadeController.createAgent("Saving Energy", "EnergyAgents.RetailerAgent", retailerContainer) );
+		retailerAgents.add( JadeController.createAgent("DayTime Energy", "EnergyAgents.RetailerAgent", retailerContainer) );
+		retailerAgents.add( JadeController.createAgent("Budget Energy", "EnergyAgents.RetailerAgent", retailerContainer) );
+		retailerAgents.add( JadeController.createAgent("Yellow Energy", "EnergyAgents.RetailerAgent", retailerContainer) );
 		
-		JadeController.showAgentGUI(retailerAgent);
+		return retailerAgents;
 		
 	}
+	
 	
 	//Create Home Agent
 	private static void createHomeAgent()
@@ -57,4 +92,15 @@ public class Main {
 			//TODO: Link with the GUI
 			//JadeController.showAgentGUI(homeAgent);
 	}
+	
+	
+	private static void createPrintAgent() {
+		JadeController.createAgent(PrintAgent.AGENT_NAME, "EnergyAgents.PrintAgent", null);
+	}
+	
+	private static void createRetailerTest() {
+		ContainerController testContainer = JadeController.createContainer("Test-Container");
+		JadeController.createAgent("RetailerTest", "EnergyAgents.RetailerTest", testContainer);
+	}
+	
 }
