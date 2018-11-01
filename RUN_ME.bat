64ab2f973c12201fb8659eee2888091ebd2c8185
Tola Veng <7678886@student.swin.edu.au>
2018-11-01 17:17:37 +1100

    Compiled v0.0.1

---
+++ RUN_ME.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+CLS
+
+echo --------------------------------------------
+
+
+
+echo Press ENTER to start
+
+set /p input=
+
+cd Compiled
+
+echo Running...
+java -jar ./HomeEnergySys.jar
+
+
+
+pause
